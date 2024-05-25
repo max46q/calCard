fa2b7534ab409953c3f118f11f7d2f8a79306b08
max46q <2000maksym2000@gmail.com>
2024-05-25 16:04:08 +0300

    в

--- app.js
+++ app.js
@@ -29,6 +29,59 @@ app.get("/", function (req, res) {
   res.render("index", { error });
 });
 
+let currentAccountIndex = 0;
+
+function nextAccount() {
+  const accounts = ["account1", "account2"]; // список ваших акаунтів
+  currentAccountIndex = (currentAccountIndex + 1) % accounts.length;
+  const accountId = accounts[currentAccountIndex];
+  window.location.href = `account.html?accountId=${accountId}`;
+}
+function addCard() {
+  const cardName = document.getElementById("cardName").value;
+  const cardLevel = document.getElementById("cardLevel").value;
+  const cardPrice = document.getElementById("cardPrice").value;
+  const cardBonus = document.getElementById("cardBonus").value;
+  // тут вам потрібно додати логіку для додавання карточки до акаунту
+  console.log(
+    `Карточка додана: ${cardName}, рівень: ${cardLevel}, ціна: ${cardPrice}, додатковий прибуток: ${cardBonus}`
+  );
+}
+
+app.post("/cards", async (req, res) => {
+  const { name, level, price, bonus, accountId } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(accountId)) {
+    return res.status(400).send("Invalid account ID");
+  }
+  const account = await User.findById(accountId);
+  if (!account) {
+    return res.status(404).send("Account not found");
+  }
+  const card = new Card({
+    name,
+    level,
+    price,
+    bonus,
+    user: account,
+  });
+  try {
+    await card.save();
+    res.redirect(`/cards/${accountId}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating card");
+  }
+});
+
+app.get("/cards/:id", async (req, res) => {
+  const id = req.params.id;
+  const account = await User.findById(id);
+  if (!account) {
+    return res.status(404).send("Акаунт не знайдено");
+  }
+  res.render("add-card", { account: account }); // Pass the account object to the template
+});
+
 const start = async () => {
   try {
     await mongoose.connect(`${process.env.DB_URL}`);
