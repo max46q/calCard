3034cfbf5ca4309719af71a39ad43a045f788d65
max46q <2000maksym2000@gmail.com>
2024-05-25 11:56:05 +0300

    xz

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Card = require("./models/card");
 const port = process.env.PORT || 3000;
-//
+
 require("dotenv").config();
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
-//
+
 app.use(express.static(__dirname + "/views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
-//
+
 app.get("/", function (req, res) {
   let error = "";
-  res.render("index", { error }); // відкриває html файл
+  res.render("index", { error });
 });
 
+app.post("/calculate", async function (req, res) {
+  const username = req.body.username;
+  try {
+    const cards = await Card.find();
+    res.render("result", { username: username, cards: cards });
+  } catch (error) {
+    console.log(error);
+    let error_msg = "Failed to fetch card data";
+    res.render("index", { error: error_msg });
+  }
+});
 
-
-console.log("server started");
 const start = async () => {
   try {
     await mongoose.connect(`${process.env.DB_URL}`);
-    app.listen(port); //port
+    app.listen(port);
     console.log("connected to db");
   } catch (e) {
     console.log(e);
   }
 };
-
 
 start();
