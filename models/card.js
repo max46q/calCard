8d95d6890e658ffb37546bfd165c5f32893fb7d9
max46q <2000maksym2000@gmail.com>
2024-05-25 12:09:33 +0300

    xx

---
+++ models/card.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  title: String,
+  level: Number,
+  price: Number,
+  additionalIncome: Number,
+});
+
+module.exports = mongoose.model("Card", cardSchema);
